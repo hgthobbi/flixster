1167c425d75fc3cb50d09a5965d2d3012e9879f4
Hemant Thobbi <hemantgthobbi@gmail.com>
2020-09-10 22:12:10 -0700

    Flix Part 2 appended
    Flix Part 2 was added to this repo

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Hemant Thobbi on 9/10/20.
+//  Copyright © 2020 Hemant Thobbi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
